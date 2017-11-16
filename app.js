6bb24d98bf91bc65bc4d1ddc368da62a4eb06de3
himanshu <himanshumehta1114@gmail.com>
2017-11-17 00:09:40 +0530

    Added / route|Convert unix to natural

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
-
+const moment = require('moment');
 var app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req,res) => {
   res.send('Home Page');
+});
+
+app.get('/:unix', (req,res) => {
+  var unix = req.params.unix;
+  try{
+    var date = moment.unix(1450137600).format('MMMM DD, YYYY');
+
+    var jsonDate = {
+      unix: Number(unix),
+      natural: date
+    };
+    res.send(jsonDate);
+  }catch(e) {
+    res.status(400).send();
+  }
 });
 
 app.listen(port, () => {
