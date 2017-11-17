2de3d81db4b34b49d19179ebe24976b372221486
himanshu <himanshumehta1114@gmail.com>
2017-11-17 12:24:34 +0530

    Conversion between unix and natural times done

--- app.js
+++ app.js
@@ -3,23 +3,25 @@ const moment = require('moment');
 var app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req,res) => {
-  res.send('Home Page');
-});
 
-app.get('/:unix', (req,res) => {
-  var unix = req.params.unix;
-  try{
-    var date = moment.unix(1450137600).format('MMMM DD, YYYY');
-
-    var jsonDate = {
-      unix: Number(unix),
-      natural: date
-    };
-    res.send(jsonDate);
-  }catch(e) {
-    res.status(400).send();
+app.get('/:userDate', (req,res) => {
+  var date = req.params.userDate;
+  if(/^\d{10}$/.test(date)){
+    date = moment(date, 'X');
+  }else{
+    date = moment(date, 'MMMM DD YYYY');
   }
+  if(date.isValid()){
+    res.json({
+      'unix' : date.format('X'),
+      'natural' : date.format('MMMM DD YYYY')
+    });
+  }else{
+    res.json({
+      'unix' : null,
+      'natural' : null
+    });
+  };
 });
 
 app.listen(port, () => {
